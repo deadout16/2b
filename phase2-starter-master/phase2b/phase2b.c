c63d067e0aa711db9da2dd94a4bbf434b58769b1
hackerrank <me@hackerrank.com>
2021-03-17 01:11:07 -0700

    Chop Suey!

--- phase2-starter-master/phase2b/phase2b.c
+++ phase2-starter-master/phase2b/phase2b.c
@@ -8,30 +8,50 @@
 
 #include "phase2Int.h"
 
+typedef struct Sleeper{
+    int WAKEUPwakeup; // Chop Suey!
+    int timeGoSleep; // when the process went to sleep
+    int pid; // process of id that went to sleep
+    int conditionId; // what is put to sleep
+    Sleeper *next;
+} Sleeper;
 
 static int      ClockDriver(void *);
 static void     SleepStub(USLOSS_Sysargs *sysargs);
 
 static int      now; // current time
+static int      clockPid;
+static int      lockId;
+
+Sleeper *head;
 
 /*
  * P2ClockInit
  *
  * Initialize the clock data structures and fork the clock driver.
  */
-void 
-P2ClockInit(void) 
-{
+void P2ClockInit(void) {
     int rc;
 
     P2ProcInit();
 
     // initialize data structures here
+    head = (Sleeper *) malloc(sizeof(Sleeper));
+    head->next = NULL;
+    head->pid = -1;
+    head->WAKEUPwakeup = -1; // Chop Suey!
+    head->timeGoSleep = -1;
+    head->conditionId = -1;
+
+    rc = P1_LockCreate("lock", &lockId);
+    assert(rc == P1_SUCCESS);
 
     rc = P2_SetSyscallHandler(SYS_SLEEP, SleepStub);
     assert(rc == P1_SUCCESS);
 
     // fork the clock driver here
+    rc = P1_Fork("clock", ClockDriver, NULL, P1_MAXSTACK, 1, clockPid);
+    assert(rc == P1_SUCCESS);
 }
 
 /*
@@ -40,9 +60,7 @@ P2ClockInit(void)
  * Clean up the clock data structures and stop the clock driver.
  */
 
-void 
-P2ClockShutdown(void) 
-{
+void P2ClockShutdown(void) {
     // stop clock driver
 }
 
@@ -51,12 +69,11 @@ P2ClockShutdown(void)
  *
  * Kernel process that manages the clock device and wakes sleeping processes.
  */
-static int 
-ClockDriver(void *arg) 
-{
+static int ClockDriver(void *arg) {
 
     while(1) {
         int rc;
+        Sleeper *temp;
 
         // wait for the next interrupt
         rc = P1_DeviceWait(USLOSS_CLOCK_DEV, 0, &now);
@@ -66,6 +83,14 @@ ClockDriver(void *arg)
         assert(rc == P1_SUCCESS);
 
         // wakeup any sleeping processes whose wakeup time has arrived
+        temp = head->next;
+        // NOTE: WAKEUP confusion?
+        while(temp != NULL || temp.WAKEUPwakeup <= now){ // Chop Suey!
+            rc = P1_Signal(temp->conditionId);
+            assert(rc == P1_SUCCESS);
+            head->next = temp->next;
+            temp = head->next;
+        }
     }
     return P1_SUCCESS;
 }
@@ -75,12 +100,36 @@ ClockDriver(void *arg)
  *
  * Causes the current process to sleep for the specified number of seconds.
  */
-int 
-P2_Sleep(int seconds) 
-{
+int P2_Sleep(int seconds) {
+    int wakeupTime;
+    int rc;
+    Sleeper *new;
+    Sleeper *temp;
     // update current time and determine wakeup time
+    int status = USLOSS_DeviceInput(USLOSS_CLOCK_DEV, 0, &now); // NOTE: 0 for second param may break
+    if (status != USLOSS_DEV_OK) {
+        USLOSS_Halt(status);
+    }
+    wakeupTime = now + (seconds * 1000);
+
     // add current process to data structure of sleepers
+    new = (Sleeper *) malloc(sizeof(Sleeper));
+    new->WAKEUPwakeup = wakeupTime; // Chop Suey!
+    new->timeGoSleep = now;
+    new->pid = P1_GetPid();
+    rc = P1_CondCreate((toString(new->pid)), lockId, &new->conditionId); // NOTE: Pointer problem maybe
+    assert(rc == P1_SUCCESS);
+
+    // add sleeper to list
+    temp = head;
+    while(temp->next != NULL || temp->next->WAKEUPwakeup <= new->WAKEUPwakeup ){ // chop Suey
+        temp = temp->next;
+    }
+    new->next = temp->next
+    temp->next = new;
+    
     // wait until it's wakeup time
+    rc = P1_Wait(new->conditionId);
     return P1_SUCCESS;
 }
 
@@ -89,9 +138,7 @@ P2_Sleep(int seconds)
  *
  * Stub for the Sys_Sleep system call.
  */
-static void 
-SleepStub(USLOSS_Sysargs *sysargs) 
-{
+static void SleepStub(USLOSS_Sysargs *sysargs) {
     int seconds = (int) sysargs->arg1;
     int rc = P2_Sleep(seconds);
     sysargs->arg4 = (void *) rc;
